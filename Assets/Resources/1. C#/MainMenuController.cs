ea1513e047d5868be84787d2426bb8ca90663f50
Maximillian-520 <maximilliankenas520@gmail.com>
2025-12-26 00:27:35 +0700

    Fixed some issues with main menu

--- Assets/Resources/1. C#/MainMenuController.cs
+++ Assets/Resources/1. C#/MainMenuController.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private Camera currentCamera;
+    public void Start()
+    {
+        currentCamera.enabled = true;
+    }
+
     public void OnPlayPressed()
     {
         SceneManager.LoadScene("Main Scene");
